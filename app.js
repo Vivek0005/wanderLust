10b82612998d2c14eb98c9e7c3f0931818198df0
Vivek <151066010+Vivek0005@users.noreply.github.com>
2024-05-25 11:13:04 +0530

    feat: Implemented index and show routes for listings and corresponding ejs templates

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 require("dotenv").config();
 const db_connection = require("./config/db.js");
 const Listing = require("./models/listing.js");
+const path = require("path");
 
 // DATABASE CONNECTION
 db_connection()
@@ -13,8 +14,26 @@ db_connection()
     console.log(err);
   });
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("Root ");
+});
+
+// INDEX ROUTE
+app.get("/listings", async (req, res) => {
+  let allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
+});
+
+// SHOW ROUTE
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
 });
 
 // PORT CONNECTION
